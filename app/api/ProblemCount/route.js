f7e16103e85d642a85f846071ea190eca77b4ab3
gopu-karthik07 <2000030281cse@gmail.com>
2024-02-05 16:10:27 +0530

    rectified problem count

--- app/api/ProblemCount/route.js
+++ app/api/ProblemCount/route.js
@@ -2,19 +2,28 @@ import { connectMongoDB } from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 import UserProb from "@/models/UserProb";
 
-
-
 export async function GET(req) {
   try {
     
     await connectMongoDB();
-    const {email} = await req?.query || {};
-    const count = await UserProb.countDocuments({email});
-    console.log(JSON.stringify(count));
+
+    const { email } = req.query || {};
+
+    
+    const count = await UserProb.countDocuments({ email });
+
+    
+    console.log(`Count for email ${email}: ${count}`);
+
+
+   
     return new NextResponse.json({ count }, { status: 200 });
   } catch (error) {
-    console.log(error);
-    return NextResponse.error(error);
-    
+    console.error("Error in GET request:", error);
+   
+    return NextResponse.error({
+      status: 500,
+      body: { error: "Internal Server Error", details: error.message },
+    });
   }
 }
